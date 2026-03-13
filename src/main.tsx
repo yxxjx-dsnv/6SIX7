02b7f1ed38f2649beca8ee3dee672479520304be
choijoshua <choijoshua16@gmail.com>
2026-03-13 11:21:10 -0400

    added json files to run website

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CampusPulse from "../campus-pulse";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <CampusPulse />
+  </React.StrictMode>
+);
